582932772c4337170014c78c9783fb7c4b55dbf0
Hayriev Bulat <bulatguy245@gmail.com>
2024-08-06 03:42:09 +0300

    8th

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import {useTelegram} from 'components/hooks/useTelegram';
+import {useTelegram} from '../components/hooks/useTelegram';
 function App() {
   const {onToggleButton, tg} = useTelegram();
 
