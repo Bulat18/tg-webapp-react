d1c461e69882b9a80459c905e12c0ca7f0f502e3
Hayriev Bulat <bulatguy245@gmail.com>
2024-08-18 17:46:51 +0300

    productlics 2

--- src/components/Productlist/Productlist.jsx
+++ src/components/Productlist/Productlist.jsx
@@ -9,11 +9,40 @@ const products = [
     {id: '3', title:'Футболка', price:3000, description:"Белого цвета, дышащая"},
     {id: '4', title:'Шорты', price:1200, description:"Черного цвета, длинные"},
     {id: '5', title:'Худи', price:2799, description:"Серого цвета, теплая"},
-    {id: '6', title:'Водоласка', price:1350, description:"Черного цвета, "},
+    {id: '6', title:'Водоласка', price:1350, description:"Черного цвета, без катышек"},
 ]
+
+const getTotalPrice = (items=[])=>{
+    return items.reduce((acc, item) =>{
+        return  acc += item.price
+    }, 0)
+}
 
 
 const Productlist = () =>{
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg}  = useTelegram ();
+    const onAdd = (product) =>{
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded){
+            newItems  = addedItems.filter(item => item.id != product.id);
+        } else{
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems);
+
+        if (newItems.lenght === 0){
+            tg.MainButton.hide();
+        } else{
+            tg.MainButton.sgow();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
     return (
         <div class="naming">
             <div className={"list"}>
