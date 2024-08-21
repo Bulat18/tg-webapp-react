a821cabd3253911c48787b4679613e3bdfedc03a
Hayriev Bulat <bulatguy245@gmail.com>
2024-08-21 22:24:14 +0300

    wear descript

--- src/components/Productlist/Productlist.jsx
+++ src/components/Productlist/Productlist.jsx
@@ -6,12 +6,12 @@ import  ProductItem from '../ProductItem/ProductItem';
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title:'Куртка', price:12000, description:"Зеленого цвета, теплая"},
+    {id: '1', title:'Куртка', price:12000, description:"Зеленоая, теплая"},
     {id: '2', title:'Джинсы', price:7000, description:"Синие, теплая"},
     {id: '3', title:'Футболка', price:3000, description:"Белого цвета, дышащая"},
     {id: '4', title:'Шорты', price:1200, description:"Черного цвета, длинные"},
     {id: '5', title:'Худи', price:2799, description:"Серого цвета, теплая"},
-    {id: '6', title:'Водоласка', price:1350, description:"Черного цвета, без катышек"},
+    {id: '6', title:'Водоласка', price:1350, description:"Черная, жарко"},
 ]
 
 const getTotalPrice = (items=[])=>{
